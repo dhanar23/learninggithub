0d8c97aca03081d02bb56bf526e1b5772a929fc7
D.rizky.K <91357430+dhanar23@users.noreply.github.com>
2023-11-02 00:21:07 +0700

    Update testpythonforgit.py

--- testpythonforgit.py
+++ testpythonforgit.py
@@ -4,7 +4,7 @@ for i in range 12:
 
 
 # change code on github again
-print("what do you want to change this code")
+# print("what do you want to change this code")
 
 def newFunction():
   return "test"
